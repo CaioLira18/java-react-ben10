6a14199a83a7ec239794fcbbb6b57957a1084f2f
CaioLira18 <caiozao1212@gmail.com>
2025-07-02 17:31:23 -0300

    [feat] - update home with a sound effect

--- ben10/src/pages/Home.jsx
+++ ben10/src/pages/Home.jsx
@@ -46,9 +46,14 @@ const Home = () => {
         <img src="https://res.cloudinary.com/dthgw4q5d/image/upload/v1751299043/808df0e1d0d32768a4a5fdf6b5e8f5ec_jbrff1.png" alt="" />
       </div>
 
+      
       <div className="aliensChoose">
         {aliens.map((alien, i) => (
-          <div key={i} className="containerAlien" onClick={() => setSelectedAlien(alien)}>
+          <div key={i} className="containerAlien" onClick={() => {
+              const audio = new Audio('/sound/Sound.mp3');
+              audio.play();
+              setSelectedAlien(alien);
+            }}>
             <div className="boxAlien">
               <img src={alien.imageHuge} alt={alien.name} />
             </div>
@@ -56,8 +61,10 @@ const Home = () => {
         ))}
       </div>
 
+
       {selectedAlien && (
         <>
+        <div className="aliensInformations">
           <div className="ominitrix">
             <div className="containerOmnitrix">
               <div className="alienHologram">
@@ -67,9 +74,8 @@ const Home = () => {
                 <img src="https://res.cloudinary.com/dthgw4q5d/image/upload/v1751293359/omnitrix_h70yjq.png" alt="Omnitrix" />
               </div>
             </div>
-          </div>
 
-          <div className="informations">
+            <div className="informations">
             <div className="containerInformations">
               <div className="alienImage">
                 <img src={selectedAlien.imageHuge} alt="" />
@@ -83,14 +89,18 @@ const Home = () => {
               </div>
             </div>
           </div>
-
+        </div>
+      </div>
+    
           <div className="alienImages">
             <img src="" alt="" />
             <img src="" alt="" />
             <img src="" alt="" />
           </div>
+           
         </>
       )}
+   
 
     
       <div className="characters">
